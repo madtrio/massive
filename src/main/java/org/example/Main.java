2a8dadfb6ddfe6aab104383b236838c3c3cb5f4b
madtrio <anna.kozitzyna@yandex.ru>
2023-09-03 20:59:03 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 public class Main {
 
     public static void main (String[] args) {
+
         int [] num = {1,2,3,4,5,6};
         System.out.println(Arrays.toString(num));
+
         for (int i = 0; i < num.length / 2; i++) {
             int out = num [i];
             num[i] = num [num.length - 1 - i];
             num[num.length - 1 - i] = out;
         }
+
         System.out.println(Arrays.toString(num));
     }
 
