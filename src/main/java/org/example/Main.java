36ebc06ad99811389cba11fab9af3860d66c7c2f
madtrio <anna.kozitzyna@yandex.ru>
2023-09-03 20:57:51 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main (String[] args) {
+        int [] num = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(num));
+        for (int i = 0; i < num.length / 2; i++) {
+            int out = num [i];
+            num[i] = num [num.length - 1 - i];
+            num[num.length - 1 - i] = out;
+        }
+        System.out.println(Arrays.toString(num));
+    }
+
+}
